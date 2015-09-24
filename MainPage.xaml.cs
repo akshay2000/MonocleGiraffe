1425c5fb9e41746ce1e353a54d031d595e02bb86
Akshay Zade <akshay2000@hotmail.com>
2015-09-24 22:57:34 +0530

    Added basic subreddit browser

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,6 +2,9 @@
 using MonocleGiraffe.Models;
 using MonocleGiraffe.Pages;
 using SharpImgur.APIWrappers;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
@@ -16,11 +19,14 @@ namespace MonocleGiraffe
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        ObservableCollection<string> subreddits = new ObservableCollection<string>();
+        List<string> subredditsList = new List<string>() { "EarthPorn", "Aww", "Funny", "Pics", "GIFs" };
         public MainPage()
         {
             InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Enabled;
             DataContext = StateHelper.ViewModel;
+            SubredditsListView.ItemsSource = subreddits;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -48,6 +54,16 @@ namespace MonocleGiraffe
             }
         }
 
+        private async void LoadSubreddit(string subreddit)
+        {
+            StateHelper.ViewModel.ImageItems.Clear();
+            var subredditGallery = await Gallery.GetSubreddditGallery(subreddit);
+            foreach (var image in subredditGallery)
+            {
+                StateHelper.ViewModel.ImageItems.Add(await GalleryItem.New(image));
+            }
+        }
+
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             MainSplitView.IsPaneOpen = !MainSplitView.IsPaneOpen;
@@ -62,6 +78,31 @@ namespace MonocleGiraffe
         {
             StateHelper.ViewModel.SelectedIndex = ImagesGridView.SelectedIndex;
             Frame.Navigate(typeof(FlipViewPage));
+        }
+
+        private async void SubredditsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (subreddits.Count > 0)
+            {
+                subreddits.Clear();
+            }
+            else
+            {
+                foreach(string subreddit in subredditsList)
+                {
+                    subreddits.Add(subreddit);
+                    await Task.Delay(50);
+                }
+            }
+        }
+
+        private void SubredditWrapper_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            string subredditName = (sender as Grid).DataContext as string;
+            PageHeaderTextBox.Text = subredditName;
+            LoadSubreddit(subredditName.ToLower());
+            subreddits.Clear();
+            MainSplitView.IsPaneOpen = false;
         }
     }
 }
