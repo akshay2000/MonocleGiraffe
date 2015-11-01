18d2a82fc2c483310c19e03553a67e029fd52140
Akshay Zade <akshay2000@hotmail.com>
2015-11-01 17:18:42 +0530

    Sharing on Album page

--- Pages/AlbumPage.xaml.cs
+++ Pages/AlbumPage.xaml.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -31,6 +33,54 @@ namespace MonocleGiraffe.Pages
             this.InitializeComponent();
             dataContext = StateHelper.AlbumVM;
             DataContext = dataContext;
+
+            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
+        }
+
+        private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            DataRequest request = args.Request;
+            var currentImage = dataContext.AlbumItem.AlbumImages[dataContext.SelectedIndex];
+            request.Data.Properties.Title = currentImage.Title ?? dataContext.AlbumItem.Title;
+            request.Data.SetWebLink(new Uri(currentImage.Link));
+        }
+
+        private void ShareMenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            string clickedTag = (string)(sender as FrameworkElement).Tag;
+            switch (clickedTag)
+            {
+                case "CopyLink":
+                    CopyLink();
+                    break;
+                case "OpenBrowser":
+                    OpenBrowser();
+                    break;
+                case "OtherApps":
+                default:
+                    DataTransferManager.ShowShareUI();
+                    break;
+            }
+        }
+
+        private void CopyLink()
+        {
+            DataPackage dataPackage = new DataPackage();
+            dataPackage.RequestedOperation = DataPackageOperation.Copy;
+            dataPackage.SetText(GetLinkToShare());
+            Clipboard.SetContent(dataPackage);
+        }
+
+        private async void OpenBrowser()
+        {
+            Uri uriToLaunch = new Uri(GetLinkToShare());
+            await Launcher.LaunchUriAsync(uriToLaunch);
+        }
+
+        private string GetLinkToShare()
+        {
+            return dataContext.AlbumItem.AlbumImages[dataContext.SelectedIndex].Link;
         }
     }
 }
