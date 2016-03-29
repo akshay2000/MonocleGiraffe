2de3fbdbef4a62a0d7faf1a4620e9da4cf8022dd
Akshay Zade <akshay2000@hotmail.com>
2016-03-29 19:20:54 +0530

    Implemented tabbed pivot headers

--- MonocleGiraffe/MonocleGiraffe/Controls/TabHeader.xaml.cs
+++ MonocleGiraffe/MonocleGiraffe/Controls/TabHeader.xaml.cs
@@ -22,7 +22,7 @@ namespace MonocleGiraffe.Controls
         public TabHeader()
         {
             this.InitializeComponent();
-            this.DataContext = this;
+            this.DataContext = this;            
         }
 
         public static readonly DependencyProperty GlyphProperty = DependencyProperty.Register("Glyph", typeof(string), typeof(TabHeader), null);
@@ -39,18 +39,42 @@ namespace MonocleGiraffe.Controls
             set { SetValue(LabelProperty, value); }
         }
 
-
-
+        public static readonly DependencyProperty IconPathProperty = DependencyProperty.Register("IconPath", typeof(Geometry), typeof(TabHeader), null);
         public Geometry IconPath
         {
             get { return (Geometry)GetValue(IconPathProperty); }
             set { SetValue(IconPathProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for IconPath.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty IconPathProperty =
-            DependencyProperty.Register("IconPath", typeof(Geometry), typeof(TabHeader), null);
 
+        public static readonly DependencyProperty PayloadProperty = DependencyProperty.Register("Payload", typeof(TabHeaderContent), typeof(TabHeader), new PropertyMetadata(null, new PropertyChangedCallback(PayloadChanged)));
+        public TabHeaderContent Payload
+        {
+            get { return (TabHeaderContent)GetValue(PayloadProperty); }
+            set { SetValue(PayloadProperty, value); }
+        }
 
+        private static void PayloadChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            TabHeader te = (TabHeader)obj;
+            TabHeaderContent newValue = (TabHeaderContent)e.NewValue;
+            te.IconPath = StringToPath(newValue.IconPath);
+            te.Label = newValue.Label;
+        }
+
+        private static Geometry StringToPath(string pathData)
+        {
+            string xamlPath =
+                "<Geometry xmlns='http://schemas.microsoft.com/winfx/2006/xaml/presentation'>"
+                + pathData + "</Geometry>";
+
+            return Windows.UI.Xaml.Markup.XamlReader.Load(xamlPath) as Geometry;
+        }
+    }
+
+    public class TabHeaderContent
+    {
+        public String Label { get; set; }
+        public String IconPath { get; set; }
     }
 }
