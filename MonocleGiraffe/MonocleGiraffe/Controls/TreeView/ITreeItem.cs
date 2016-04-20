9dfba0beb1b31d46635953b548793649a58282e1
Akshay Zade <akshay2000@hotmail.com>
2016-04-20 22:53:44 +0530

    Enhanced image and album templates

---
+++ MonocleGiraffe/MonocleGiraffe/Controls/TreeView/ITreeItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonocleGiraffe.Controls.TreeView
+{
+    public interface ITreeItem
+    {
+        object Content { get; set; }
+        List<ITreeItem> Children { get; set; }
+    }    
+}
