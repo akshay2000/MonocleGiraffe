4954140618b5be8fa92d54e5652164bccb072264
Akshay Zade <akshay2000@hotmail.com>
2016-05-03 23:06:27 +0530

    mend

---
+++ MonocleGiraffe/MonocleGiraffe/Converters/BooleanInverseConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace MonocleGiraffe.Converters
+{
+    public class BooleanInverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool inValue = (bool)value;
+            return !inValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
