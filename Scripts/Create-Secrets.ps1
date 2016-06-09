413750be67b6a19d371345391b109e3f64f5ae00
Akshay Zade <akshay2000@hotmail.com>
2016-06-09 12:51:24 +0530

    Added script to generate Secrets.json

---
+++ Scripts/Create-Secrets.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+    [string]$outPath,
+    [string]$clientId,
+    [string]$mashApeKey,
+    [string]$clientSecret
+)
+"{
+  `"Client_Id`": `"$clientId`",
+  `"Mashape_Key`": `"$mashApeKey`",
+  `"Client_Secret`": `"$clientSecret`"
+}" | Out-File -FilePath $outPath
