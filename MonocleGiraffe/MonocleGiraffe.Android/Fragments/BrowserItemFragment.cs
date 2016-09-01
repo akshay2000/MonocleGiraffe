fa253a1741978437697786882b76b2e0f21c2817
Akshay Zade <akshay2000@hotmail.com>
2016-09-01 12:49:02 +0530

    Video view POC for animations

--- MonocleGiraffe/MonocleGiraffe.Android/Fragments/BrowserItemFragment.cs
+++ MonocleGiraffe/MonocleGiraffe.Android/Fragments/BrowserItemFragment.cs
@@ -13,6 +13,7 @@ using Android.Widget;
 using MonocleGiraffe.Portable.Models;
 using FFImageLoading.Views;
 using FFImageLoading;
+using Android.Media;
 
 namespace MonocleGiraffe.Android.Fragments
 {
@@ -35,18 +36,64 @@ namespace MonocleGiraffe.Android.Fragments
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            var layout = isAlbum ? Resource.Layout.Tmpl_Album : Resource.Layout.Tmpl_Image;
+            int layout;// = isAlbum ? Resource.Layout.Tmpl_Album : Resource.Layout.Tmpl_Image;
+            switch (item.ItemType)
+            {
+                case GalleryItemType.Album:
+                    layout = Resource.Layout.Tmpl_Album;
+                    break;
+                case GalleryItemType.Animation:
+                    layout = Resource.Layout.Ctrl_Image;
+                    break;
+                default:
+                    layout = Resource.Layout.Tmpl_Image;
+                    break;
+
+            }
             // Use this to return your custom view for this Fragment
             return inflater.Inflate(layout, container, false);
         }
 
         public override void OnViewCreated(View view, Bundle savedInstanceState)
         {
-            if (!isAlbum)
-                ImageService.Instance.LoadUrl(item.Link).Into(MainImage);
-            Title.Text = item.Title;
+            switch (item.ItemType)
+            {
+                case GalleryItemType.Album:
+                    Title.Text = item.Title;
+                    break;
+                case GalleryItemType.Animation:
+                    MainVideoView.SetVideoURI(global::Android.Net.Uri.Parse(item.Mp4));
+                    MainVideoView.Start();
+                    MainVideoView.Prepared += MainVideoView_Prepared;
+                    break;
+                default:
+                    Title.Text = item.Title;
+                    ImageService.Instance.LoadUrl(item.Link).Into(MainImage);
+                    break;
+
+            }
+            //if (!isAlbum)
+            //    ImageService.Instance.LoadUrl(item.Link).Into(MainImage);
+            //Title.Text = item.Title;
             base.OnViewCreated(view, savedInstanceState);
         }
+
+        private void MainVideoView_Prepared(object sender, EventArgs e)
+        {
+            var mp = sender as MediaPlayer;
+            mp.Looping = true;
+        }
+
+        private VideoView mainVideoView;
+        public VideoView MainVideoView
+        {
+            get
+            {
+                mainVideoView = mainVideoView ?? View.FindViewById<VideoView>(Resource.Id.MainVideoView);
+                return mainVideoView;
+            }
+        }
+
 
         private ImageViewAsync mainImage;
         public ImageViewAsync MainImage
