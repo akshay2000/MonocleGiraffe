4e49e918b246edb6ac4510e36f80b2e47cd01461
Akshay Zade <akshay2000@hotmail.com>
2017-06-21 21:18:35 +0530

    Tile pinning for subreddits

--- MonocleGiraffe/MonocleGiraffe/Pages/SubGalleryPage.xaml.cs
+++ MonocleGiraffe/MonocleGiraffe/Pages/SubGalleryPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using MonocleGiraffe.Portable.Models;
+using MonocleGiraffe.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.StartScreen;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -28,10 +32,83 @@ namespace MonocleGiraffe.Pages
             NavigationCacheMode = NavigationCacheMode.Enabled;
         }
 
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await Task.Delay(100);
+            var sub = (DataContext as SubGalleryPageViewModel).Sub;
+            string tileId = sub.Url;
+            ToggleAppBarButton(!SecondaryTile.Exists(tileId));
+        }
+
         public void ScrollMe(object sender, object parameter)
         {
             int selectedIndex = (int)((parameter as DependencyPropertyChangedEventArgs).NewValue);
             MainGridView.ScrollIntoView(MainGridView.Items[selectedIndex]);
+        }
+
+        private async void TogglePin(SubredditItem subreddit, Rect rect)
+        {
+            string tileId = subreddit.Url;
+            if (SecondaryTile.Exists(tileId))
+            {
+                // Unpin
+                SecondaryTile secondaryTile = new SecondaryTile(tileId);
+                Windows.UI.Popups.Placement placement = Windows.UI.Popups.Placement.Above;
+                bool isUnpinned = await secondaryTile.RequestDeleteForSelectionAsync(rect, placement);
+                ToggleAppBarButton(isUnpinned);
+            }
+            else
+            {
+                string displayName = $"/r/{subreddit.Url}";
+                string tileActivationArguments = tileId;
+                Uri logo = new Uri("ms-appx:///Assets/Square150x150Logo.png");
+                TileSize newTileDesiredSize = TileSize.Square150x150;
+
+                SecondaryTile secondaryTile = new SecondaryTile(tileId, displayName, tileId, logo, newTileDesiredSize);
+
+                secondaryTile.VisualElements.ShowNameOnSquare150x150Logo = true;
+                Windows.UI.Popups.Placement placement = Windows.UI.Popups.Placement.Above;
+                if (!(Windows.Foundation.Metadata.ApiInformation.IsTypePresent(("Windows.Phone.UI.Input.HardwareButtons"))))
+                {
+                    bool isPinned = await secondaryTile.RequestCreateForSelectionAsync(rect, placement);
+                    ToggleAppBarButton(!isPinned);
+                }
+
+                //Phone
+                if ((Windows.Foundation.Metadata.ApiInformation.IsTypePresent(("Windows.Phone.UI.Input.HardwareButtons"))))
+                {
+                    await secondaryTile.RequestCreateAsync();
+                }
+            }
+        }
+
+        private void ToggleAppBarButton(bool showPinButton)
+        {
+            if (showPinButton)
+            {
+                TogglePinButton.Label = "Pin to Start";
+                TogglePinButton.Icon = new SymbolIcon(Symbol.Pin);
+            }
+            else
+            {
+                TogglePinButton.Label = "Unpin from Start";
+                TogglePinButton.Icon = new SymbolIcon(Symbol.UnPin);
+            }
+            TogglePinButton.UpdateLayout();
+        }
+
+        private static Rect GetElementRect(FrameworkElement element)
+        {
+            GeneralTransform buttonTransform = element.TransformToVisual(null);
+            Point point = buttonTransform.TransformPoint(new Point());
+            return new Rect(point, new Size(element.ActualWidth, element.ActualHeight));
+        }
+
+        private void TogglePinButton_Click(object sender, RoutedEventArgs e)
+        {
+            Rect rect = GetElementRect((FrameworkElement)sender);
+            TogglePin(((FrameworkElement)sender).DataContext as SubredditItem, rect);
         }
     }
 }
