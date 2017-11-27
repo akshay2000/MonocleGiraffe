73c3fc163c9a2afef598f4e9fdfffcf30d2ca0b8
Akshay Zade <akshay2000@hotmail.com>
2017-11-27 22:01:20 +0530

    Added post clone script for app center

---
+++ MonocleGiraffe/MonocleGiraffe/appcenter-post-clone.ps1
@@ -0,0 +1,10 @@
+﻿"{
+  `"Client_Id`": `"$env:CLIENT_ID`",
+  `"Mashape_Key`": `"$env:MASHAPE_KEY`",
+  `"Client_Secret`": `"$env:CLIENT_SECRET`",
+  `"Ad_Config`" :
+  {
+	`"App_Key`": `"$env:ADDUPLEX_APP_KEY`",
+	`"Banner_Id`": `"$env:ADDUPLEX_BANNER_ID`"
+  }
+}" | Out-File -FilePath Secrets.json
