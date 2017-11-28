b322abea17b3899ad06b6909cf968d36b3b771dc
Akshay Zade <akshay2000@hotmail.com>
2017-11-28 22:04:14 +0530

    Ugh again!

--- MonocleGiraffe/appcenter-post-clone.ps1
+++ MonocleGiraffe/appcenter-post-clone.ps1
@@ -12,23 +12,23 @@
 Write-Output "Operating from source directory $env:APPCENTER_SOURCE_DIRECTORY"
 
 $inPath = '.\MonocleGiraffe\MonocleGiraffe.sln'
-# $lines = Get-Content -Path .\input.txt
-# Clear-Content $inPath
-# $uid = "undefined"
-# for ($i = 0; $i -lt $lines.Count; $i++) {
-#     $line = $lines[$i]
-#     if ($uid -eq "undefined" -and $line -match 'MonocleGiraffe.Android') {
-#         $split = $line.Split(',')
-#         $uid = $split[2].Trim().Trim('"')
-#         $i++
-#         continue
-#     } elseif ($line -match $uid) {
-#         continue
-#     } else {
-#         Add-Content $inPath $line
-#     }
-# }
+$lines = Get-Content -Path .\input.txt
+Clear-Content $inPath
+$uid = "undefined"
+for ($i = 0; $i -lt $lines.Count; $i++) {
+    $line = $lines[$i]
+    if ($uid -eq "undefined" -and $line -match 'MonocleGiraffe.Android') {
+        $split = $line.Split(',')
+        $uid = $split[2].Trim().Trim('"')
+        $i++
+        continue
+    } elseif ($line -match $uid) {
+        continue
+    } else {
+        Add-Content $inPath $line
+    }
+}
 
-Write-Output "Contents of SLN\n\n"
+Write-Output 'Contents of SLN'
 
 Get-Content $inPath
