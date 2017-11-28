de23248e08e81c1c693d103e419fa8e420e3b641
Akshay Zade <akshay2000@hotmail.com>
2017-11-28 16:59:40 +0530

    Script to change remove Android project from solution

---
+++ MonocleGiraffe/appcenter-post-clone.ps1
@@ -0,0 +1,28 @@
+﻿"{
+  `"Client_Id`": `"$env:CLIENT_ID`",
+  `"Mashape_Key`": `"$env:MASHAPE_KEY`",
+  `"Client_Secret`": `"$env:CLIENT_SECRET`",
+  `"Ad_Config`" :
+  {
+	`"App_Key`": `"$env:ADDUPLEX_APP_KEY`",
+	`"Banner_Id`": `"$env:ADDUPLEX_BANNER_ID`"
+  }
+}" | Out-File -FilePath .\MonocleGiraffe\MonocleGiraffe\Secrets.json
+
+$inPath = '.\MonocleGiraffe\MonocleGiraffe.sln'
+$lines = Get-Content -Path .\input.txt
+Clear-Content $inPath
+$uid = "undefined"
+for ($i = 0; $i -lt $lines.Count; $i++) {
+    $line = $lines[$i]
+    if ($uid -eq "undefined" -and $line -match 'MonocleGiraffe.Android') {
+        $split = $line.Split(',')
+        $uid = $split[2].Trim().Trim('"')
+        $i++
+        continue
+    } elseif ($line -match $uid) {
+        continue
+    } else {
+        Add-Content $inPath $line
+    }
+}
