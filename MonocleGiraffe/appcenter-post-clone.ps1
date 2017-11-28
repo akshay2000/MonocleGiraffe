f1dd5cb5f97a53d31be9c0ca81f3e60ad89a7641
Akshay Zade <akshay2000@hotmail.com>
2017-11-28 21:40:27 +0530

    Removed Android project from solution

--- MonocleGiraffe/appcenter-post-clone.ps1
+++ MonocleGiraffe/appcenter-post-clone.ps1
@@ -9,20 +9,26 @@
   }
 }" | Out-File -FilePath .\MonocleGiraffe\MonocleGiraffe\Secrets.json
 
+Write-Output "Operating from source directory $env:APPCENTER_SOURCE_DIRECTORY"
+
 $inPath = '.\MonocleGiraffe\MonocleGiraffe.sln'
-$lines = Get-Content -Path .\input.txt
-Clear-Content $inPath
-$uid = "undefined"
-for ($i = 0; $i -lt $lines.Count; $i++) {
-    $line = $lines[$i]
-    if ($uid -eq "undefined" -and $line -match 'MonocleGiraffe.Android') {
-        $split = $line.Split(',')
-        $uid = $split[2].Trim().Trim('"')
-        $i++
-        continue
-    } elseif ($line -match $uid) {
-        continue
-    } else {
-        Add-Content $inPath $line
-    }
-}
+# $lines = Get-Content -Path .\input.txt
+# Clear-Content $inPath
+# $uid = "undefined"
+# for ($i = 0; $i -lt $lines.Count; $i++) {
+#     $line = $lines[$i]
+#     if ($uid -eq "undefined" -and $line -match 'MonocleGiraffe.Android') {
+#         $split = $line.Split(',')
+#         $uid = $split[2].Trim().Trim('"')
+#         $i++
+#         continue
+#     } elseif ($line -match $uid) {
+#         continue
+#     } else {
+#         Add-Content $inPath $line
+#     }
+# }
+
+Write-Output "Contents of SLN\n\n"
+
+Get-Content $inPath
